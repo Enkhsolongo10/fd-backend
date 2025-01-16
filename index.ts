c0b36b4fd8f4b77a5f58493a2c231464c2a5a823
24LP1557 24LP1557 <24LP1557@G305-CC0109.local>
2025-01-16 17:59:36 +0800

    food category CRUD

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { configDotenv } from "dotenv"; //ene n .env dotroos medeelel awah zorilgotoi ym shig bn
 import express, { Request, Response } from 'express'; //end bga express-iig can't get it!!!!
+import {Collection, connect, model, Schema} from "mongoose";
 
-const PORT = 4000;
+const mongoose = require('mongoose');
+const PORT = 8000;
 const app = express();
 app.use(express.json());
 
@@ -18,81 +20,47 @@ export const connectToDb = async () => {
         );
     };
     
+    try{
+        await mongoose.connect(URI_fromenv);
+        console.log('Succesfully connected to the MongoDB database.');
+    } catch(error){
+        console.error('Failed to connect the MongoDB database:', error);
+        process.exit(1); 
+    };
 };
+connectToDb(); 
 
-app.get('/', async (req: Request, res: Response) => {
-    res.send('Hello world!');
-}); 
+const FOOD_CATEGORY_SCHEMA = new Schema(
+    {
+        categoryName: String,
+    },
+    {timestamps: true}
+);
+
+const FoodCategoryModel = model('FoodCategory', FOOD_CATEGORY_SCHEMA, 'food-category');
+
+app.get('/food-category/', async (req: Request, res: Response) => {
+    const allData = await FoodCategoryModel.find(); 
+    res.json(allData);
+});
+
+app.get('/food-category/:id', async (req: Request, res: Response) => {
+});
+
+app.get('/create', async (req: Request, res: Response) => {
+    //create new food category 
+    const newItem = await FoodCategoryModel.create({
+        categoryName: 'fast food'
+    });
+
+    res.send({
+        message: 'New food category created successfully.',
+        newItem
+    });
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is Running on http://localhost:${PORT}`);
 }); 
-
-
-
-
-
-
-// import { configDotenv } from 'dotenv';
-// import express, { Request, Response } from 'express';
-// import { connect, model, Schema } from 'mongoose';
-
-// const mongoose = require("mongoose");
-
-// const PORT = 8000;
-// const app = express();
-
-// configDotenv();
-
-// const connectToDB = async () => {
-//     const MONGODB_URI = process.env.MONGODB_URI;
-
-//     if(!MONGODB_URI) {
-//         throw new Error(
-//             "Database Connection URI is not defined in environment variables."
-//         );
-//     };
-
-//     try{
-//         await mongoose.connect(MONGODB_URI);
-//         console.log('Succesfully connected to the MongoDB database.');
-//     } catch(error){
-//         console.error('Failed to connect the MongoDB database:', error);
-//         process.exit(1); 
-//     }
-// }
-
-// connectToDB();
-
-// const FOOD_CATEGORY_SCHEMA = new mongoose.Schema({
-//     categoryName: String,
-//     id: Number
-// });
-
-// const FoodCategoryModel = mongoose.model('FoodCategory', FOOD_CATEGORY_SCHEMA);
-
-// app.use(express.json());
-
-// app.get('/food-category/', async (req: Request, res: Response ) => {
-//     const foodCategories = await FoodCategoryModel.find();
-//     console.log('Food Categories', foodCategories);
-//     res.send('Hello from backend');
-// });
-// app.get('/food-category/:id', async (req: Request, res: Response ) => {
-//     res.send('Hello from backend');
-// });
-// app.post('/food-category/', async (req: Request, res: Response ) => {
-//     res.send('Hello from backend');
-// })
-// app.delete('/food-category/:id', async (req: Request, res: Response ) => {
-//     res.send('Hello from backend');
-// });
-// app.put('/food-category/:id', async (req: Request, res: Response ) => {
-//     res.send('Hello from backend');
-// });
-
-
-
-// app.listen(PORT, () => {
-//     console.log(`Server is Running on http:localhost`)
-// }); 
